d2e430e18736b68632abb34985b824b3e1b296d4
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-11-24 16:17:14 +0300

    testmonials

---
+++ client/src/components/Descriptions.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../assets/assets';
+
+const Descriptions = () => {
+  return (
+    <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
+      <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
+      <p className='text-gray-500 mb-8'>Trun your imagination into visuals</p>
+
+      <div className='flex flex-col gap-5 md:gap-14 md:flex-row items-center'>
+        <img className='w-80 xl:w-96 rounded-lg' src={assets.sample_img_1} alt="sample image one" />
+
+        <div>
+          <h2 className='text-3xl font-medium max-w-lg mb-4'> Introducing the AI-Powered Text to Image Generator</h2>
+          <p className='text-gray-600 mb-4'>"Introducing the AI-powered text-to-image generator, a cutting-edge tool that effortlessly transforms your words into stunning, high-quality visuals. Bring your ideas to life with precision and creativity like never before."</p>
+          <p className='text-gray-600'>"Unleash your creativity with the AI-powered text-to-image generator. Turn simple text into visually stunning, detailed images in moments, perfect for any project or inspiration."</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Descriptions
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
 import Steps from "../components/Steps";
+import Descriptions from "../components/Descriptions";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return <div>
     <Header/>
     <Steps/>
+    <Descriptions/>
+    <Testimonials/>
   </div>;
 };
 
