76e19f8f77d5c10d333c95ee0ea7554bed81aaae
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-11-26 20:12:59 +0300

    resiult  added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,14 +3,18 @@ import Header from "../components/Header";
 import Steps from "../components/Steps";
 import Descriptions from "../components/Descriptions";
 import Testimonials from "../components/Testimonials";
+import GenerateBtn from "../components/GenerateBtn";
 
 const Home = () => {
-  return <div>
-    <Header/>
-    <Steps/>
-    <Descriptions/>
-    <Testimonials/>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <Steps />
+      <Descriptions />
+      <Testimonials />
+      <GenerateBtn />
+    </div>
+  );
 };
 
 export default Home;
